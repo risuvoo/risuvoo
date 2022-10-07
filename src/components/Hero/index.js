8fe51c1cc0392bd790993ca656ac91738dcf1ac4
meXuvo <rafiqulislamsuvobd@gmail.com>
2022-10-08 02:21:13 +0600

    changfes

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function Hero() {
   return (
-    <section id="hero" className="mexuvo-hero w-full">
+    <section id="hero" className="mexuvo-hero w-full md:mt-0 mt-10">
       <div className="container mx-auto pl-5 lg:pl-10">
-        <div className="w-full h-screen relative">
+        <div className="w-full md:h-screen relative">
           <div className="flex flex-col h-full md:items-start items-center justify-center lg:flex-row lg:items-center lg:space-x-10 w-full xl:justify-start xl:space-x-24">
             {/* profile img */}
             <div className="mexuvo-profile bg-white border-8 border-white rounded-full overflow-hidden mb-10 lg:mb-0">
@@ -33,13 +33,24 @@ function Hero() {
                   Under Construction
                 </h1>
               </div>
-              <div className="mexuvo-btn mexuvo-hire-btn w-full h-full">
-                <a
-                  href="#"
-                  className="w-64 h-12 inline-flex rounded-tr-xl transition duration-300 ease-in-out rounded-bl-xl justify-center items-center bg-mexuvo-red-500 text-white uppercase text-sm tracking-wide"
-                >
-                  Emergency
-                </a>
+              <div className="flex md:flex-row flex-col space-y-0 md:space-y-0 md:space-x-5 space-x-0">
+                <div className="mexuvo-btn mexuvo-hire-btn mb-5">
+                  <a
+                    href="#"
+                    className="w-64 h-12 inline-flex rounded-tr-xl transition duration-300 ease-in-out rounded-bl-xl justify-center items-center bg-mexuvo-red-500 text-white uppercase text-sm tracking-wide"
+                  >
+                    Emergency
+                  </a>
+                </div>
+                <div className="mexuvo-btn mexuvo-download-btn w-full">
+                  <a
+                    download
+                    href="../../assets/file/suvo-resume.pdf"
+                    className="w-64 h-12 inline-flex rounded-tr-xl uppercase transition duration-300 ease-in-out rounded-bl-xl justify-center items-center border-2 text-shark-500 hover:text-white hover:bg-shark-500 border-shark-500 text-sm tracking-wide"
+                  >
+                    Download CV
+                  </a>
+                </div>
               </div>
             </div>
           </div>
